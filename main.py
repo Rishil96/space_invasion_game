66e27c47fd6816bd27e16d2193b0875adff5797c
Rishil Ramesh <rishil.ramesh@outlook.com>
2022-11-25 17:39:54 +0530

    Add : Add screen, title and icon for the game.

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 # Space Invasion Game
 import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Display the screen
+pygame.display.set_mode(size=(800, 600))
+
+# Title and Icon
+pygame.display.set_caption("Space Invasion")
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
+
+# Game loop
+is_running = True
+
+while is_running:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            pygame.display.quit()
+            pygame.quit()
+            is_running = False
