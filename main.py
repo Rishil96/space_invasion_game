1b27a3961cab440892cb6577c10f00aac292f440
Rishil Ramesh <rishil.ramesh@outlook.com>
2022-11-25 17:58:13 +0530

    Add : Add a new player to the screen

--- main.py
+++ main.py
@@ -12,14 +12,29 @@ pygame.display.set_caption("Space Invasion")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
+# Create player
+player_image = pygame.image.load("Rocket.png")
+player_x = 368
+player_y = 536
+
+
+def player():
+    screen.blit(player_image, (player_x, player_y))
+
+
 # Game loop
 is_running = True
 
 while is_running:
+    # RGB Background
+    screen.fill((205, 144, 228))
+
     for event in pygame.event.get():
         if event == pygame.QUIT:
             pygame.display.quit()
             pygame.quit()
             is_running = False
-    screen.fill((205, 144, 228))
+
+    player()
+
     pygame.display.update()
